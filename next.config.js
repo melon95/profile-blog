f45a4c738075317defeb1d3b85871284b39be34a
melon95 <lijunwork95@gmail.com>
2022-10-07 18:47:59 +0800

    优化Docker镜像和部署

--- next.config.js
+++ next.config.js
@@ -6,11 +6,12 @@ const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [],
-    rehypePlugins: [],
-  },
+    rehypePlugins: []
+  }
 })
 module.exports = withMDX({
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  output: 'standalone'
 })
