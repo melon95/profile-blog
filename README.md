e719ff2f4313fba77f9d47d87d1bda26b115cdc9
melon95 <lijunwork95@gmail.com>
2022-10-13 10:53:43 +0800

    feat: 添加License

--- README.md
+++ README.md
@@ -13,3 +13,7 @@ docker build -t next-blog:latest .
 ```shell
 docker-compose up -d
 ```
+
+## License
+
+代码采用 [MIT](https://github.com/melon95/next-blog/main/LICENSE) 许可, 文章采用 [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/) 许可。
--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,12 @@
 const Footer = () => {
   return (
     <footer className="h-14 flex justify-center items-center border-t">
-      <span>Copyright © melon95.All rights reserved.</span>
+      <span>
+        <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh">
+          CC BY-NC-SA 4.0
+        </a>
+        © melon95.
+      </span>
       <span className="ml-8">
         Designed and Proudly powered by Next、Docker、Tailwind.
       </span>
